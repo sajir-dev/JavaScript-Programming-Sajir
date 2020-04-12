49ecab156ab8fa1bc73e4038b8ecb4af0cfa5a69
Sajir T <sajirt@gmail.com>
2020-04-12 09:13:07 +0530

    011 Multiplication Table upto 12

---
+++ 011_MultiplicationTable.js
@@ -0,0 +1,19 @@
+multiplicationtable = (n, index) => {
+    for (let i = 1; i <= index; i++) {
+      console.log(n + "X" + i + "=" + n * i);
+    }
+    return "end";
+  };
+  
+  console.log(multiplicationtable(1, 10));
+  console.log(multiplicationtable(2, 10));
+  console.log(multiplicationtable(3, 10));
+  console.log(multiplicationtable(4, 10));
+  console.log(multiplicationtable(5, 10));
+  console.log(multiplicationtable(6, 10));
+  console.log(multiplicationtable(7, 10));
+  console.log(multiplicationtable(8, 10));
+  console.log(multiplicationtable(9, 10));
+  console.log(multiplicationtable(10, 10));
+  console.log(multiplicationtable(11, 10));
+  console.log(multiplicationtable(12, 10));
