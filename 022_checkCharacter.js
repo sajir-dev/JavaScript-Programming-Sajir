b92c10738c19ca55cdfad3118ccb0352f6288865
Sajir T <sajirt@gmail.com>
2020-04-12 10:10:42 +0530

    check the character

---
+++ 022_checkCharacter.js
@@ -0,0 +1,12 @@
+checkcharacter = (ch) => {
+  switch (true) {
+    case "A" <= ch && ch <= "Z":
+      return "Given character is an uppercase letter";
+    case "c" <= ch && ch <= "z":
+      return "Given character is a lowercase letter";
+    case "!" <= ch && ch <= "@":
+      return "Given character is a special character";
+    default:
+      return "The given character is neither an alphabet nor a special character under ASCII standards";
+  }
+};
