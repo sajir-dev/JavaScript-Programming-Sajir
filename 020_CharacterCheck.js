b0d9bd27f0e96522fb9e0494155c90ac987c4487
Sajir T <sajirt@gmail.com>
2020-04-12 10:01:09 +0530

    020 check character

---
+++ 020_CharacterCheck.js
@@ -0,0 +1,9 @@
+charactercheck = (ch) => {
+  if ((ch >= "a" && ch <= "z") || (ch >= "A" && ch <= "Z")) {
+    return "The given character is an alphabet";
+  } else {
+    return "The given character is not an alphabet";
+  }
+};
+
+console.log(charactercheck("5"));
