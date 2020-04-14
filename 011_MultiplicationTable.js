236e82618acc2d61388df225e78f81fb6847f645
sajir-dev <62017128+sajir-dev@users.noreply.github.com>
2020-04-14 12:12:34 +0530

    Update 011_MultiplicationTable.js

--- 011_MultiplicationTable.js
+++ 011_MultiplicationTable.js
@@ -1,6 +1,6 @@
 multiplicationtable = () => {
   let table = ""; let ch = '  '
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 12; i++) {
     for (let j = 1; j <= 10; j++) {
       if ((i * j).toString.length === 1) {
         ch = '    ';
