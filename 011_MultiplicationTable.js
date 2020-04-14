e2cc905b3285416a0699d9b1fb23815dc2db9361
sajir-dev <62017128+sajir-dev@users.noreply.github.com>
2020-04-14 11:15:36 +0530

    space inserted between variable declarations

--- 011_MultiplicationTable.js
+++ 011_MultiplicationTable.js
@@ -1,5 +1,5 @@
 multiplicationtable = () => {
-  let table = "";let ch = '  '
+  let table = ""; let ch = '  '
   for (let i = 1; i <= 10; i++) {
     for (let j = 1; j <= 10; j++) {
       if ((i * j).toString.length === 1) {
