19ce598e18ac6f9d30a44e4ed5b17df6eb0ee034
sajir-dev <62017128+sajir-dev@users.noreply.github.com>
2020-04-16 09:35:16 +0530

    Code alignment fixed

--- 018_leapyears.js
+++ 018_leapyears.js
@@ -1,10 +1,10 @@
 next20Leapyears = () => {
-let i = 2024;
-let leapYears = [];
-while (i <= 2100) {
-  leapYears.push(i);
-  i = i + 4;
-}
+  let i = 2024;
+  let leapYears = [];
+  while (i <= 2100) {
+    leapYears.push(i);
+    i = i + 4;
+  }
   return leapYears;
 }
 
