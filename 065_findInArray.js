ffc62c11066674484df78bb0a9cdf28cc4e8d60f
sajir-dev <62017128+sajir-dev@users.noreply.github.com>
2020-04-16 09:40:31 +0530

    Alignment adjusted

--- 065_findInArray.js
+++ 065_findInArray.js
@@ -1,10 +1,8 @@
-const arrayFind = (arr, x) => {
-    
+const arrayFind = (arr, x) => {   
     for (let i = 0; i<arr.length ; i++) {
       if (arr[i]===x) return true;
     }
     return false;
-  };
-  
-  console.log(arrayFind([12, 3, 5, 10], 100));
+};
+console.log(arrayFind([12, 3, 5, 10], 100));
   
